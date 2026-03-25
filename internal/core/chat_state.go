daf1cc225a79befb5450146468c50c5f1dd6ac01
music-miko <voilet856@gmail.com>
2026-03-26 01:30:39 +0530

    Update chat_state.go

--- internal/core/chat_state.go
+++ internal/core/chat_state.go
@@ -1,18 +1,8 @@
 /*
- * ● YukkiMusic
+ * ● ArcMusic
  * ○ A high-performance engine for streaming music in Telegram voicechats.
  *
- * Copyright (C) 2026 TheTeamVivek
- *
- * This program is free software: you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * Repository: https://github.com/TheTeamVivek/YukkiMusic
+ * Copyright (C) 2026 Team Arc
  */
 
 package core
